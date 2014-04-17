b7d4abe33b84049398f7e3ec5cc8f43259b3e2a2
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-17 15:48:22 +0200

    Fix type

--- db/migrate/20140417094429_create_events.rb
+++ db/migrate/20140417094429_create_events.rb
@@ -1,25 +1,25 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string  :lastfm_uuid
-      t.string  :title,                  null: false
-      t.string  :lastfm_url
-      t.string  :website
-      t.string  :venue_name
-      t.string  :venue_latitude,         null: false
-      t.string  :venue_longitude,        null: false
-      t.string  :venue_city
-      t.string  :venue_country
-      t.string  :venue_street
-      t.string  :venue_postalcode
-      t.string  :venue_url
-      t.date    :starts_at,              null: false
-      t.date    :ends_at,                null: false
-      t.string  :lastfm_image_small
-      t.string  :lastfm_image_medium
-      t.string  :lastfm_image_large
-      t.string  :lastfm_image_extralarge
-      t.string  :poster,                 null: false
+      t.string   :lastfm_uuid
+      t.string   :title,                  null: false
+      t.string   :lastfm_url
+      t.string   :website
+      t.string   :venue_name
+      t.string   :venue_latitude,         null: false
+      t.string   :venue_longitude,        null: false
+      t.string   :venue_city
+      t.string   :venue_country
+      t.string   :venue_street
+      t.string   :venue_postalcode
+      t.string   :venue_url
+      t.datetime :starts_at,              null: false
+      t.datetime :ends_at,                null: false
+      t.string   :lastfm_image_small
+      t.string   :lastfm_image_medium
+      t.string   :lastfm_image_large
+      t.string   :lastfm_image_extralarge
+      t.string   :poster,                 null: false
 
       t.timestamps
     end
