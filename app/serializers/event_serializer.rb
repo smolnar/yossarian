940dcbb29dd5f3c110b5e5eafc8f8534085a835d
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-19 23:30:24 +0200

    Fix serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :poster, :venue_latitude, :venue_longitude
+  attributes :id, :title, :poster, :venue_latitude, :venue_longitude, :starts_at, :ends_at
 
   has_many :artists, embed: :ids, include: true
 end
