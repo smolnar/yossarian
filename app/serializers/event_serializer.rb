eb9ac5d1b5710169d0fcc17af9f6f5c0ddfd2706
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-20 14:48:46 +0200

    Extend EventSerializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :poster, :venue_latitude, :venue_longitude, :starts_at, :ends_at
+  attributes :id, :title, :poster, :venue_name, :venue_city, :venue_country, :venue_latitude, :venue_longitude, :starts_at, :ends_at
 
   has_many :artists, embed: :ids, include: true
 end
