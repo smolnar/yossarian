9d34d280cd98fe1b561e42fadba1e06f9d4ba576
Samuel Molnar <molnar.samuel@gmail.com>
2014-04-24 15:48:18 +0200

    Extend API::EventsController spec

--- spec/controllers/api/events_controller_spec.rb
+++ spec/controllers/api/events_controller_spec.rb
@@ -2,12 +2,33 @@ require 'spec_helper'
 
 describe API::EventsController do
   describe 'GET index' do
-    let!(:artist) { event.artists.first }
-    let!(:event) { create :event, :with_artists}
-    let!(:recordings) { 2.times.map { create :recording, artist: artist }}
+    let(:events) { 3.times.map { create :event } }
+    let(:artists_with_image) { 3.times.map { create :artist, image: fixture('poster.jpg') }}
+    let(:artists_without_image) { 3.times.map { create :artist, image: nil }}
 
-    it 'returns serialized events' do
+    before :each do
+      artists_with_image[0..1].each do |artist|
+        create :recording,   artist: artist
+        create :performance, artist: artist, event: events.first
+      end
+
+      create :recording,   artist: artists_with_image.last, youtube_url: nil
+      create :performance, artist: artists_with_image.last, event: events.first
+
+      artists_without_image.each do |artist|
+        create :recording,   artist: artist, youtube_url: nil
+        create :performance, artist: artist, event: events.second
+      end
+    end
+
+    it 'returns serialized events having artists image and youtube links' do
       get :index, format: :json
+
+      @events = assigns(:events)
+
+      expect(@events.size).to eql(1)
+      expect(@events.first).to eql(events.first)
+      expect(@events.first.artists.sort).to eql(artists_with_image[0..1].sort)
 
       expect(response).to be_success
     end
