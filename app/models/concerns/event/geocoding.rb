4871fc83386a3260a738cf529f240cc5b806beba
Samuel Molnar <molnar.samuel@gmail.com>
2014-05-20 20:24:44 +0200

    Fix geocoding module

---
+++ app/models/concerns/event/geocoding.rb
@@ -0,0 +1,16 @@
+class Event
+  module Geocoding
+    extend ActiveSupport::Concern
+
+    included do
+      unless Rails.env.test?
+        reverse_geocoded_by :venue_latitude, :venue_longitude do |record, results|
+          if data = results.first
+            record.venue_city    = data.city unless record.venue_city.present?
+            record.venue_country = data.country unless record.venue_country.present?
+          end
+        end
+      end
+    end
+  end
+end
