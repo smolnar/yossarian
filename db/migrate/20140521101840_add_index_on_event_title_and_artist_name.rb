8d885362298bc8dc3572efa765593b1eb31b19c5
Samuel Molnar <molnar.samuel@gmail.com>
2014-05-22 18:12:35 +0200

    Fix index names

--- db/migrate/20140521101840_add_index_on_event_title_and_artist_name.rb
+++ db/migrate/20140521101840_add_index_on_event_title_and_artist_name.rb
@@ -1,23 +1,23 @@
 class AddIndexOnEventTitleAndArtistName < ActiveRecord::Migration
   def up
     execute <<-SQL
-      CREATE INDEX index_events_unaccented_lowercased_title
+      CREATE INDEX index_events_on_unaccented_lowercased_title
       ON events (lower(musicbrainz_unaccent(title)));
     SQL
 
     execute <<-SQL
-      CREATE INDEX index_artists_unaccented_lowercased_name
+      CREATE INDEX index_artists_on_unaccented_lowercased_name
       ON artists (lower(musicbrainz_unaccent(name)));
     SQL
   end
 
   def down
     execute <<-SQL
-      DROP INDEX index_events_unaccented_lowercased_title;
+      DROP INDEX index_events_on_unaccented_lowercased_title;
     SQL
 
     execute <<-SQL
-      DROP INDEX index_artists_unaccented_lowercased_name;
+      DROP INDEX index_artists_on_unaccented_lowercased_name;
     SQL
   end
 end
