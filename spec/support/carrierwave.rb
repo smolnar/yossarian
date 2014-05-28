57d90d9ded0998b9915d42c25946226dd49d7a54
Samuel Molnar <molnar.samuel@gmail.com>
2014-05-28 13:54:55 +0200

    Use custom location for Carrierwave when testing

---
+++ spec/support/carrierwave.rb
@@ -0,0 +1,15 @@
+if defined?(Carrierwave)
+  CarrierWave::Uploader::Base.descendants.each do |uploader|
+    next if uploader.anonymous?
+
+    uploader.class_eval do
+      def cache_dir
+        "#{Rails.root}/spec/support/uploads/tmp"
+      end
+
+      def store_dir
+        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
+    end
+  end
+end
